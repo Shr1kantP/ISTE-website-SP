abbe4d858cd6031e30458d738703ce3f15b7b7f6
Shrikant2116 <pawarshrikant244@gmail.com>
2024-12-12 23:13:31 +0530

    just commit

--- overall.js
+++ overall.js
@@ -4,3 +4,14 @@ registerButton.addEventListener("click", () => {
 
     window.location.href = "event-page.html";
 });
+
+// Optimized Code
+window.addEventListener('load', () => {
+    const images = document.querySelectorAll('img');
+    images.forEach(image => {
+      image.addEventListener('click', (event) => {
+        // Do something on click, using event.target
+      });
+    });
+  });
+  
