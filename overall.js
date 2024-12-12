2e405facfe9a6834d847eefdb5f52fc9c18e16d4
Shrikant2116 <pawarshrikant244@gmail.com>
2024-12-13 00:29:12 +0530

    #

--- overall.js
+++ overall.js
@@ -2,16 +2,15 @@ const registerButton = document.getElementById("registerButton");
 
 registerButton.addEventListener("click", () => {
 
-    window.location.href = "event-page.html";
+  window.location.href = "event-page.html";
 });
 
 // Optimized Code
 window.addEventListener('load', () => {
-    const images = document.querySelectorAll('img');
-    images.forEach(image => {
-      image.addEventListener('click', (event) => {
-        // Do something on click, using event.target
-      });
+  const images = document.querySelectorAll('img');
+  images.forEach(image => {
+    image.addEventListener('click', (event) => {
+      // Do something on click, using event.target
     });
   });
-  
+});
